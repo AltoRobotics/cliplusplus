fd79deda352c642ef5f8ce7446a53246a8318b91
Giuseppe Iellamo <giuseppe.iellamo@altorobotics.ai>
2023-03-09 15:46:36 +0100

    true, false implemented

--- README.md
+++ README.md
@@ -5,7 +5,16 @@ In test le asserzioni che testano che i comandi siano eseguiti e che l'output si
 
 Per semplicitá al momento i test che si invocano con 
 
-    ninja test 
+    make test 
+
+o
+
+    ninja test
 
 ma cercano gli eseguibili sotto ../src : tenetene conto se modificate la build.
 
+# Implementation
+
+**true**  https://man7.org/linux/man-pages/man1/true.1.html - senza option
+
+**false**  https://man7.org/linux/man-pages/man1/false.1.html - senza option
--- test/tests.cpp
+++ test/tests.cpp
@@ -12,3 +12,18 @@ TEST_CASE("Runs")
   auto exit_status = process1.get_exit_status();
   REQUIRE(process1.get_exit_status() == 0);
 }
+
+TEST_CASE("True")
+{
+  Process process1("../src/true", "", [](const char *bytes, size_t n) {});
+  auto exit_status = process1.get_exit_status();
+  REQUIRE(process1.get_exit_status() == 0);
+}
+
+
+TEST_CASE("False")
+{
+  Process process1("../src/false", "", [](const char *bytes, size_t n) {});
+  auto exit_status = process1.get_exit_status();
+  REQUIRE(process1.get_exit_status() == 1);
+}
